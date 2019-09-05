d7b712d87f1f87cbb3ce7e6a3f54cb3d5f057e4f
Camilo Andrés Moreno Laverde <camolave12@gmail.com>
2019-09-05 18:42:32 -0500

    Add files via upload

---
+++ dspl-archetype/src/main/java/com/dspl/archetype/DsplArchetypeApplication.java
@@ -0,0 +1,13 @@
+package com.dspl.archetype;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsplArchetypeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsplArchetypeApplication.class, args);
+	}
+
+}
