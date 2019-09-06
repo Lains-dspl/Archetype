c740af3e5cc2c53f217af9d53fa798051ea9e308
Camilo Andrés Moreno Laverde <camolave12@gmail.com>
2019-09-06 05:37:53 -0500

    Add files via upload

--- dspl-archetype/src/main/java/com/dspl/archetype/DsplArchetypeApplication.java
+++ dspl-archetype/src/main/java/com/dspl/archetype/DsplArchetypeApplication.java
@@ -2,8 +2,14 @@ package com.dspl.archetype;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
+@ComponentScan(basePackages = { "com.dspl.app.core.*" })
+@EnableMongoRepositories(basePackages = { "com.dspl.app.core.*" })
+@EntityScan(basePackages = { "com.dspl.app.core.*" })
 public class DsplArchetypeApplication {
 
 	public static void main(String[] args) {
---
+++ dspl-archetype/target/b.bat
@@ -0,0 +1,1 @@
+java -jar dspl-archetype-0.0.1-SNAPSHOT.jar
